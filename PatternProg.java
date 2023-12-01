d2f4087a81e737d8f56655e196a1a49300d5a423
akankshagite072 <150998236+akankshagite072@users.noreply.github.com>
2023-12-01 11:57:54 +0530

    Create PatternProg.java

---
+++ PatternProg.java
@@ -0,0 +1,29 @@
+class PatternProg
+
+{
+
+public static void main(String ar
+
+gs[])
+
+{
+
+for(int i=1; i<=5; i++)
+
+{
+
+for(int j=5; j>=i; j--)
+
+{
+
+System.out.print("*");}
+
+System.out.print("*");
+}
+System.out.println();
+
+}
+
+}
+
+}
